177a3c4538905d8d7838c16193d1c7deacbe9502
iamthirteeen@gmail.com <a8765432>
2018-10-03 20:37:15 +0800

    configruation

--- example/_config.py
+++ example/_config.py
@@ -1,7 +1,7 @@
 # GET CLIENT_ID, CLIENT_SECRET on http://cubepay.io
-CLIENT_ID = '3874216a29c69352f3d5576c4ece090c'
-CLIENT_SECRET = '6RGgjhRCadwqtj4PeqKkuf8PucPKKJKKRsLtDgNx'
+CLIENT_ID = 'Your CLIENT ID'
+CLIENT_SECRET = 'Your CLIENT SECRET'
 
-# Live mode : https://api.cubepay.io
+ # Live mode : https://api.cubepay.io
 # Sandbox mode : https://api.sandbox.cubepay.io
 URL = 'https://api.cubepay.io'
